d2ae98aab0798229115a4e9188ac1ea6c77be821
Abra7am <ibrahimyertum@gmail.com>
2025-11-29 22:47:59 +0300

    Task 2. AWS IaC with Terraform: Create resources for SSH Authentication

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,40 @@
+data "aws_vpc" "cmtr_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["cmtr-wz7heak5-vpc"]
+  }
+}
+
+data "aws_subnet" "public_a" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.cmtr_vpc.id]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-1a"]
+  }
+}
+
 data "aws_security_group" "cmtr_sg" {
   filter {
     name   = "group-name"
     values = ["cmtr-wz7heak5-sg"]
   }
-}
 
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.cmtr_vpc.id]
+  }
+}
 resource "aws_instance" "cmtr_ec2" {
-  ami           = "ami-0fa3fe0fa7920f68e"
+  ami           = "ami-0fa3fe0fa7920f68ec"
   instance_type = "t2.micro"
 
-  key_name               = aws_key_pair.cmtr_keypair.key_name
+  key_name = aws_key_pair.cmtr_keypair.key_name
+
+  subnet_id              = data.aws_subnet.public_a.id
   vpc_security_group_ids = [data.aws_security_group.cmtr_sg.id]
 
   tags = {
